6d5373a74e49ea279096164ba2d1c6a6ac798245
tomasis7 <tomasis7@gmail.com>
2024-11-08 22:09:28 +0100

    Refactor HTML structure, remove unused file, and enhance JavaScript functionality with dynamic content loading.
    IT works now. The initial stage thoug. PRoblem with sizes of images and text.

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
 window.addEventListener("DOMContentLoaded", main);
 
 function main() {
-  loadStartScene();
+  const container = document.getElementById("container");
+  loadStartScene(container);
 }
 
-function loadStartScene() {
+function loadStartScene(container) {
   container.innerHTML = "";
 
-  const text = document.createElement("p");
-  text.textContent = ".......";
+  const text = document.createElement("h1");
+  text.textContent = "Welcome to the game";
   text.className = "large green";
 
-  const button = document.createElement("button");
-  button.textContent = "Börja!";
-  button.onclick = loadKitchenScene;
-
-  container.append(text, button);
-}
-
-function loadKitchenScene() {
-  container.innerHTML = "";
-
   const image1 = document.createElement("img");
-  image1.src = "images/kitchen.gif";
+  image1.src = "images/playbutton.gif";
+  image1.onclick = () => loadRoom0Scene(container);
 
   const image2 = document.createElement("img");
-  image2.src = "images/kitchen2.gif";
+  image2.src = "images/skeletonhand.gif";
 
-  container.append(image1, image2);
+  container.append(text, image1, image2);
+}
+
+function loadRoom0Scene(container) {
+  container.innerHTML = "";
+
+  const image3 = document.createElement("img");
+  image3.src = "images/room0.gif";
+
+  const image4 = document.createElement("img");
+  image4.src = "images/rightarrowbutton.gif";
+
+  container.append(image3, image4);
 }
