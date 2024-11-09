81883efee8f3f3b5efa4fd65964ed279acdff984
Tomas Maldonis <tomasis7@gmail.com>
2024-11-09 11:23:06 +0100

    Refactor HTML structure, remove unused files, and implement new JavaScript functionality for dynamic scene loading
    Adding various refactoring alternatives suggested by Copilot in order to simplify the whole code.

--- main.js
+++ main.js
@@ -42,119 +42,92 @@ function loadStartScene(container) {
 function loadRoom0Scene(container) {
   container.innerHTML = "";
 
-  const images = [
-    {
-      src: "images/leftarrowbutton.gif",
-      width: 100,
-      onclick: () => loadStartScene(container),
-    },
-    { src: "images/room0.gif" },
-    {
-      src: "images/rightarrowbutton.gif",
-      width: 100,
-      onclick: () => loadRoom1Scene(container),
-    },
-  ];
+  const image3 = document.createElement("img");
+  image3.src = "images/leftarrowbutton.gif";
+  image3.width = 100;
+  image3.onclick = () => loadStartScene(container);
+
+  const image4 = document.createElement("img");
+  image4.src = "images/room0.gif";
+
+  const image5 = document.createElement("img");
+  image5.src = "images/rightarrowbutton.gif";
+  image5.width = 100;
+  image5.onclick = () => loadRoom1Scene(container);
 
   container.style.display = "flex";
   container.style.justifyContent = "center";
   container.style.alignItems = "center";
   container.style.flexDirection = "row";
 
-  images.forEach((imageData) => {
-    const img = document.createElement("img");
-    img.src = imageData.src;
-    if (imageData.width) img.width = imageData.width;
-    if (imageData.onclick) img.onclick = imageData.onclick;
-    container.appendChild(img);
-  });
+  container.append(image3, image4, image5);
 }
-
 function loadRoom1Scene(container) {
   container.innerHTML = "";
 
-  const images = [
-    {
-      src: "images/leftarrowbutton.gif",
-      width: 100,
-      onclick: () => loadRoom0Scene(container),
-    },
-    { src: "images/room1.gif" },
-    {
-      src: "images/rightarrowbutton.gif",
-      width: 100,
-      onclick: () => loadRoom2Scene(container),
-    },
-  ];
+  const image3 = document.createElement("img");
+  image3.src = "images/leftarrowbutton.gif";
+  image3.width = 100;
+  image3.onclick = () => loadRoom0Scene(container);
+
+  const image6 = document.createElement("img");
+  image6.src = "images/room1.gif";
+
+  const image5 = document.createElement("img");
+  image5.src = "images/rightarrowbutton.gif";
+  image5.width = 100;
+  image5.onclick = () => loadRoom2Scene(container);
 
   container.style.display = "flex";
   container.style.justifyContent = "center";
   container.style.alignItems = "center";
   container.style.flexDirection = "row";
 
-  images.forEach((imageData) => {
-    const img = document.createElement("img");
-    img.src = imageData.src;
-    if (imageData.width) img.width = imageData.width;
-    if (imageData.onclick) img.onclick = imageData.onclick;
-    container.appendChild(img);
-  });
+  container.append(image3, image6, image5);
 }
-
 function loadRoom2Scene(container) {
   container.innerHTML = "";
 
-  const images = [
-    {
-      src: "images/leftarrowbutton.gif",
-      width: 100,
-      onclick: () => loadRoom1Scene(container),
-    },
-    { src: "images/room2.gif" },
-    {
-      src: "images/rightarrowbutton.gif",
-      width: 100,
-      onclick: () => loadRoom3Scene(container),
-    },
-  ];
+  const image3 = document.createElement("img");
+  image3.src = "images/leftarrowbutton.gif";
+  image3.width = 100;
+  image3.onclick = () => loadRoom1Scene(container);
+
+  const image7 = document.createElement("img");
+  image7.src = "images/room2.gif";
+
+  const image5 = document.createElement("img");
+  image5.src = "images/rightarrowbutton.gif";
+  image5.width = 100;
+  image5.onclick = () => loadRoom3Scene(container);
 
   container.style.display = "flex";
   container.style.justifyContent = "center";
   container.style.alignItems = "center";
   container.style.flexDirection = "row";
 
-  images.forEach((imageData) => {
-    const img = document.createElement("img");
-    img.src = imageData.src;
-    if (imageData.width) img.width = imageData.width;
-    if (imageData.onclick) img.onclick = imageData.onclick;
-    container.appendChild(img);
-  });
+  container.append(image3, image7, image5);
 }
-
 function loadRoom3Scene(container) {
   container.innerHTML = "";
 
-  const images = [
-    {
-      src: "images/leftarrowbutton.gif",
-      width: 100,
-      onclick: () => loadRoom2Scene(container),
-    },
-    { src: "images/room3.gif" },
-    { src: "images/rightarrowbutton.gif", width: 100 },
-  ];
+  const image3 = document.createElement("img");
+  image3.src = "images/leftarrowbutton.gif";
+  image3.width = 100;
+  image3.onclick = () => loadRoom2Scene(container);
+
+  const image8 = document.createElement("img");
+  image8.src = "images/room3.gif";
+
+  const image5 = document.createElement("img");
+  image5.src = "images/rightarrowbutton.gif";
+  image5.width = 100;
+  image5.onclick = () => loadRoom4Scene(container);
 
   container.style.display = "flex";
   container.style.justifyContent = "center";
   container.style.alignItems = "center";
   container.style.flexDirection = "row";
 
-  images.forEach((imageData) => {
-    const img = document.createElement("img");
-    img.src = imageData.src;
-    if (imageData.width) img.width = imageData.width;
-    if (imageData.onclick) img.onclick = imageData.onclick;
-    container.appendChild(img);
-  });
+  container.append(image3, image7, image5);
 }
---
+++ main_implementing_array.js
@@ -0,0 +1,151 @@
+container.innerHTML = "";
+
+const text = document.createElement("h1");
+text.textContent = "Welcome to the game";
+text.className = "large green";
+
+const image1 = document.createElement("img");
+image1.src = "images/playbutton.gif";
+image1.width = 100;
+image1.height = 100;
+image1.onclick = () => loadRoom0Scene(container);
+
+const image2 = document.createElement("img");
+image2.src = "images/skeletonhand.gif";
+image2.width = 100;
+
+const text1 = document.createElement("p");
+text1.textContent = "Submit your soul to play";
+text1.className = "large green";
+
+const input = document.createElement("input");
+input.type = "text";
+input.placeholder = "Enter your name";
+input.className = "large green";
+
+container.style.display = "flex";
+container.style.justifyContent = "center";
+container.style.alignItems = "center";
+container.style.flexDirection = "column";
+
+container.append(text, image1, image2, text1, input);
+
+function loadRoom0Scene(container) {
+  container.innerHTML = "";
+
+  const images = [
+    {
+      src: "images/leftarrowbutton.gif",
+      width: 100,
+      onclick: () => loadStartScene(container),
+    },
+    { src: "images/room0.gif" },
+    {
+      src: "images/rightarrowbutton.gif",
+      width: 100,
+      onclick: () => loadRoom1Scene(container),
+    },
+  ];
+
+  container.style.display = "flex";
+  container.style.justifyContent = "center";
+  container.style.alignItems = "center";
+  container.style.flexDirection = "row";
+
+  images.forEach((imageData) => {
+    const img = document.createElement("img");
+    img.src = imageData.src;
+    if (imageData.width) img.width = imageData.width;
+    if (imageData.onclick) img.onclick = imageData.onclick;
+    container.appendChild(img);
+  });
+}
+
+function loadRoom1Scene(container) {
+  container.innerHTML = "";
+
+  const images = [
+    {
+      src: "images/leftarrowbutton.gif",
+      width: 100,
+      onclick: () => loadRoom0Scene(container),
+    },
+    { src: "images/room1.gif" },
+    {
+      src: "images/rightarrowbutton.gif",
+      width: 100,
+      onclick: () => loadRoom2Scene(container),
+    },
+  ];
+
+  container.style.display = "flex";
+  container.style.justifyContent = "center";
+  container.style.alignItems = "center";
+  container.style.flexDirection = "row";
+
+  images.forEach((imageData) => {
+    const img = document.createElement("img");
+    img.src = imageData.src;
+    if (imageData.width) img.width = imageData.width;
+    if (imageData.onclick) img.onclick = imageData.onclick;
+    container.appendChild(img);
+  });
+}
+
+function loadRoom2Scene(container) {
+  container.innerHTML = "";
+
+  const images = [
+    {
+      src: "images/leftarrowbutton.gif",
+      width: 100,
+      onclick: () => loadRoom1Scene(container),
+    },
+    { src: "images/room2.gif" },
+    {
+      src: "images/rightarrowbutton.gif",
+      width: 100,
+      onclick: () => loadRoom3Scene(container),
+    },
+  ];
+
+  container.style.display = "flex";
+  container.style.justifyContent = "center";
+  container.style.alignItems = "center";
+  container.style.flexDirection = "row";
+
+  images.forEach((imageData) => {
+    const img = document.createElement("img");
+    img.src = imageData.src;
+    if (imageData.width) img.width = imageData.width;
+    if (imageData.onclick) img.onclick = imageData.onclick;
+    container.appendChild(img);
+  });
+}
+
+function loadRoom3Scene(container) {
+  container.innerHTML = "";
+
+  const images = [
+    {
+      src: "images/leftarrowbutton.gif",
+      width: 100,
+      onclick: () => loadRoom2Scene(container),
+    },
+    { src: "images/room3.gif" },
+    { src: "images/rightarrowbutton.gif", width: 100 },
+  ];
+
+  container.style.display = "flex";
+  container.style.justifyContent = "center";
+  container.style.alignItems = "center";
+  container.style.flexDirection = "row";
+
+  images.forEach((imageData) => {
+    const img = document.createElement("img");
+    img.src = imageData.src;
+    if (imageData.width) img.width = imageData.width;
+    if (imageData.onclick) img.onclick = imageData.onclick;
+    container.appendChild(img);
+  });
+}
